38ee41761c1e214a55610165d9b93a6f517a391d
wfswee <wfswee@chicagobooth.edu>
2021-05-18 04:12:16 +0000

    migrated transactions date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_025552) do
+ActiveRecord::Schema.define(version: 2021_05_18_040728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_05_18_025552) do
     t.bigint "owner_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "date", null: false
     t.index ["owner_id"], name: "index_transactions_on_owner_id"
     t.index ["vendor"], name: "index_transactions_on_vendor"
   end
