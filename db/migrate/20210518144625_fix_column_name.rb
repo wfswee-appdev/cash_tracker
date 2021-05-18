8c130575a9f3c0abc5b35a49de385435f5f7d9ab
wfswee <wfswee@chicagobooth.edu>
2021-05-18 14:51:17 +0000

    changed migration name

--- db/migrate/20210518144625_fix_column_name.rb
+++ db/migrate/20210518144625_fix_column_name.rb
@@ -1,4 +1,4 @@
-class FixVendorName < ActiveRecord::Migration[6.1]
+class FixColumnName < ActiveRecord::Migration[6.1]
   def change
     rename_column :transactions, :vendor, :category, default: "Lunch", null: False, index: True
   end
